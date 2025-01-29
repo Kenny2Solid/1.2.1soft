efa34639c2acb57c2310684538af5a01db154f91
Kendrick Torrance <torraken000@warren.k12.in.us>
2025-01-29 12:47:20 -0500

    yooo

--- src/main.java
+++ src/main.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
-
 public class main {
+
+    public class RowColumnSum {
+        public static void main(String[] args) {
+            int[][] matrix = {
+                    {1, 2, 3},
+                    {4, 5, 6},
+                    {7, 8, 9}
+            };
+
+            int rows = matrix.length;
+            int cols = matrix[0].length;
+
+            // Calculate row sums
+            System.out.println("Row sums:");
+            for (int i = 0; i < rows; i++) {
+                int rowSum = 0;
+                for (int j = 0; j < cols; j++) {
+                    rowSum += matrix[i][j];
+                }
+                System.out.println("Row " + (i + 1) + ": " + rowSum);
+            }
+
+            // Calculate column sums
+            System.out.println("\nColumn sums:");
+            for (int j = 0; j < cols; j++) {
+                int colSum = 0;
+                for (int i = 0; i < rows; i++) {
+                    colSum += matrix[i][j];
+                }
+                System.out.println("Column " + (j + 1) + ": " + colSum);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
 }
